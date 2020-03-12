05af0619ac6d9a46f1e2f4701f10479b2eca98cf
weihsu00 <ericshe21110@gmail.com>
2020-03-12 15:59:17 +0800

    Not modulized.

--- scapy_udptest.py
+++ scapy_udptest.py
@@ -18,30 +18,52 @@ class UdpSend():
 		self.payload = ""
 
 	def payload_handler(self):
-		# rand_payload = ''.join(random.choice(ascii_letters) for i in range(random.randint(1, 65536)))
-		# rand_payload = ''.join(random.choice(ascii_letters) for i in range(0, 65501))
-		rand_payload = os.urandom(65507)
+		rand_payload = os.urandom(65000)
 		return rand_payload
 
 	def forge_packet(self):
-		# self.packet.len = 0xffff
-		# self.packet.chksum = None
-		# self.payload = self.payload_handler()
-		pass
+		if args.Tcase:
+			if args.Tcase == 1:
+				self.packet.len = random.randint(0,7)
+			if args.Tcase == 2:
+				self.packet.len = 65535
+			if args.Tcase == 3:
+				self.packet.len = random.randint(41,32766)
+			if args.Tcase == 4:
+				self.packet.len = random.randint(9,39)
+			if args.Tcase == 5:
+				self.packet.len = random.randint(32767, 65534)
+			if args.Tcase == 6:
+				self.packet.chksum = random.randint(0,65535)
+			if args.Tcase == 7:
+				pass
+			if args.Tcase == 8:
+				pass
+		else:		
+			# self.packet.len = 7
+			# self.packet.chksum = None
+			# self.payload = self.payload_handler()
+			# self.payload = "KKKK"
+			pass
 
 
 	def start_sending(self):
 		self.forge_packet()
 		# print(self.packet.show())
+		print("[*] Send DNS query(UDP)")
 		if self.payload:
-			print("[*] Send Raw")
-			send(self.ipheader/self.packet/self.payload)
+			response, unans = sr(self.ipheader/self.packet/DNS(rd=1, qd=DNSQR(qname='www.iii.org.tw'))/self.payload, timeout=1, verbose=0)	
 		else:
-			print("[*] Send DNS")
-			response, unans = sr(self.ipheader/self.packet/DNS(rd=1, qd=DNSQR(qname='www.iii.org.tw')), timeout=2, verbose=0)	
+			response, unans = sr(self.ipheader/self.packet/DNS(rd=1, qd=DNSQR(qname='www.iii.org.tw')), timeout=1, verbose=0)	
+
+
+		if response:
 			print(response[0][0][UDP].show())
 			print(response[DNS].summary())
-			print(hexdump(response[0][0]))
+			# print(hexdump(response[0][0]))
+		else:
+			print("No response from DUT")
+			
 
 if __name__ == '__main__':
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -51,16 +73,19 @@ if __name__ == '__main__':
 	s.close()
 
 	parser = ArgumentParser()
-	parser.add_argument("-r", "--rand",help="Make packet random if set.", action="store_true")
 	parser.add_argument("-v", "--verbose", help="Show verbosity.", action="store_true")
+	parser.add_argument("-T", "--Tcase", help="EDSA test cases.", type=int, choices=range(1,9))
+	parser.add_argument("-c", "--count", help="How many packets to send(not for flooding cases).", type=int, default=1)
 	parser.add_argument("destIP", help="Destination device IP.", type=str)
 	parser.add_argument("destPort", help="Destination device port.", type=int)
 	args = parser.parse_args()
 
 	DUT_ip = args.destIP
 	DUT_port = args.destPort
-	port_num = random.randrange(49152, 2**16)
-	print("[*] Local port number:", port_num)
-	udp_snd = UdpSend(DUT_ip, local_ip, DUT_port, port_num)
-	udp_snd.start_sending()
+	start_port = random.randrange(49152, 2**16-args.count)
+	for packet_num, port_num in enumerate(range(start_port, start_port+args.count)):
+			print("---------Packet %s -----------" % packet_num)
+			print("[*] Local port number:", port_num)
+			udp_snd = UdpSend(DUT_ip, local_ip, DUT_port, port_num)
+			udp_snd.start_sending()
 	
